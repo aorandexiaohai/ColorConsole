7f04ddfb157bfdb759820c881082f654d3ee3342
wuzhenhai <you@example.com>
2018-11-09 06:28:05 -0800

    code

--- ColorConsole.h
+++ ColorConsole.h
@@ -16,6 +16,19 @@ enum Color
     WHITE = 0xFFFFFF,
     BLACK = 0x000000
 };
+
+// red=     "\e[0;31m"
+// RED=     "\e[1;31m"
+// blue=    "\e[0;34m"
+// BLUE=    "\e[1;34m"
+// cyan=    "\e[0;36m"
+// CYAN=    "\e[1;36m"
+// green=   "\e[0;32m"
+// GREEN=   "\e[1;32m"
+// yellow=  "\e[0;33m"
+// YELLOW=  "\e[1;33m"
+// NC=      "\e[0m"
+
 class ColorConsole : public std::stringstream
 {
   private:
@@ -46,8 +59,23 @@ class ColorConsole : public std::stringstream
         //reback to old color
         SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
         #else
+        //linux
+        if (color == RED)
+        {
+            std::cout << "\e[1;31m";
+        }
+        else if(color == GREEN)
+        {
+            std::cout << "\e[0;32m";
+        }
+        else if(color == BLUE)
+        {
+            std::cout << "\e[1;34m";
+        }
         std::cout << this->rdbuf();
         std::cout.flush();
+        
+        std::cout << "\e[0m";
         #endif
     }
 };
